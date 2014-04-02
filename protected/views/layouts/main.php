d7272273e3074983b847a58bc059ab367bf3ff34
PaulK <PaulK@Paul.ikonsoft.ro>
2014-04-02 12:41:00 +0300

    cleanup si butoane

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -30,10 +30,14 @@
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
-				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+				//array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
+				array('label'=>'Portofolio', 'url'=>array('/site/contact')),
+                                array('label'=>'Services', 'url'=>array('/site/contact')),
+                                array('label'=>'Products', 'url'=>array('/site/contact')),
+                                array('label'=>'Process', 'url'=>array('/site/contact')),
+                                array('label'=>'Contact Us', 'url'=>array('/site/contact')),
+				//array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
+				//array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
 		)); ?>
 	</div><!-- mainmenu -->
@@ -48,9 +52,9 @@
 	<div class="clear"></div>
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+		Copyright &copy; <?php echo date('Y'); ?> by ProjectZero@Varu <br/>
 		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
+		<?php //echo Yii::powered(); ?> 
 	</div><!-- footer -->
 
 </div><!-- page -->
