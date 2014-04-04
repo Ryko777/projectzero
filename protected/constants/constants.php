f6c57df576e5a0aa2752184ee5c502d8dacd2d6e
decemvre <decemvre@gmail.com>
2014-04-04 12:00:11 +0300

    created constants folder and  added colors

---
+++ protected/constants/constants.php
@@ -0,0 +1,12 @@
+<?php
+
+// kraftsoft colors
+define("white", "rgb(255,255,255)");
+define("darkblue", "rgb(2,21,26)");
+define("mainblue", "rgb(4,58,71)");
+define("lightblue", "rgb(8,120,145)");
+define("lightgray", "rgb(200,200,200)");
+define("activelink", "rgb(179,29,20)");
+define("visitedlink", "rgb(0,0,0)"); // with underline, perhaps not final
+
+?>
